10df30cbd3e488e349a3183277d3c94ad3fb4fdf
Sun Peng <sunpeng1995@gmail.com>
2018-05-12 14:04:40 +0800

    feat: hit sphere

---
+++ include/ray.h
@@ -0,0 +1,21 @@
+#ifndef __RAY_H__
+#define __RAY_H__
+#include "vec3.h"
+
+class ray {
+public:
+    ray() {}
+    ray(const vec3 &origin, const vec3 &direction) {
+        O = origin;
+        D = direction;
+    }
+    vec3 origin() const { return O; }
+    vec3 direction() const { return D; }
+    vec3 pointAtParameter(float t) const { return O + t * D; }
+
+  private:
+    vec3 O;
+    vec3 D;
+};
+
+#endif
